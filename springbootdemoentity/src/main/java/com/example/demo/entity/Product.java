17ce0fcf69911e855c03169ff2682aee27208cd6
xjz <273904882@qq.com>
2019-09-17 12:18:38 +0800

    eureka 提交

---
+++ springbootdemoentity/src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,43 @@
+package com.example.demo.entity;
+
+/**
+ * Created by Administrator on 2019/9/17.
+ */
+public class Product {
+    private String name;
+    private int age;
+    private String add;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAdd() {
+        return add;
+    }
+
+    public void setAdd(String add) {
+        this.add = add;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
